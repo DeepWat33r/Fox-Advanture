2e10c8f01b3b6889938b1208a68eeabac455c165
Denys <denis.gidzun@gmail.com>
2023-02-12 17:42:43 +0200

    del comm

--- Assets/Scripts/CharacterController2D.cs
+++ Assets/Scripts/CharacterController2D.cs
@@ -37,11 +37,10 @@ public class CharacterController2D : MonoBehaviour
 
 	private void FixedUpdate()
 	{
+		
 		var wasGrounded = _grounded;
 		_grounded = false;
 
-		// The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
-		// This can be done using layers instead but Sample Assets will not overwrite your project settings.
 		var colliders = Physics2D.OverlapCircleAll(groundCheck.position, GroundedRadius, ground);
 		for (int i = 0; i < colliders.Length; i++)
 		{
@@ -52,10 +51,8 @@ public class CharacterController2D : MonoBehaviour
 	
 	public void Move(float move, bool crouch, bool jump)
 	{
-		// If the character has a ceiling preventing them from standing up, keep them crouching
 		if (Physics2D.OverlapCircle(ceilingCheck.position, CeilingRadius, ground))
 			crouch = true;
-		// If crouching
 		if (crouch)
 		{
 			if (!_wasCrouching)
@@ -63,12 +60,12 @@ public class CharacterController2D : MonoBehaviour
 				_wasCrouching = true;
 				onCrouchEvent.Invoke(true);
 			}
-			move *= crouchSpeed; 								// Reduce the speed by the crouchSpeed multiplier
-			if (crouchDisableCollider != null)					// Disable one of the colliders when crouching
+			move *= crouchSpeed; 								
+			if (crouchDisableCollider != null)					
 				crouchDisableCollider.enabled = false;
 		} else
 		{
-			if (crouchDisableCollider != null)					// Enable the collider when not crouching
+			if (crouchDisableCollider != null)					
 				crouchDisableCollider.enabled = true;
 			if (_wasCrouching)
 			{
@@ -76,14 +73,10 @@ public class CharacterController2D : MonoBehaviour
 				onCrouchEvent.Invoke(false);
 			}
 		}
-		// Move the character by finding the target velocity
 		Vector3 targetVelocity = new Vector2(move * 10f, _rb.velocity.y);
-		// And then smoothing it out and applying it to the character
 		_rb.velocity = Vector3.SmoothDamp(_rb.velocity, targetVelocity, ref _velocity, movementSmoothing);
-		// If the player should jump...
 		if (_grounded && jump)
 		{
-			// Add a vertical force to the player.
 			_grounded = false;
 			_rb.AddForce(new Vector2(0f, jumpForce));
 		}
@@ -98,10 +91,7 @@ public class CharacterController2D : MonoBehaviour
 	}
 	private void Flip()
 	{
-		// Switch the way the player is labelled as facing.
 		_facingRight = !_facingRight;
-
-		// Multiply the player's x local scale by -1.
 		Vector3 theScale = transform.localScale;
 		theScale.x *= -1;
 		transform.localScale = theScale;
