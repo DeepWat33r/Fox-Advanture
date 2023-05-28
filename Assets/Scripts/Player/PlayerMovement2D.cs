6a8385da21d0a9346f1d3f8c817568a8257e0640
Denys <denis.gidzun@gmail.com>
2023-05-29 00:58:53 +0300

    Player code fixes

--- Assets/Scripts/Player/PlayerMovement2D.cs
+++ Assets/Scripts/Player/PlayerMovement2D.cs
@@ -2,53 +2,63 @@ using UnityEngine;
 
 namespace Player
 {
-    public class PlayerMovement2D : MonoBehaviour {
-
+    public class PlayerMovement2D : MonoBehaviour 
+    {
+        // Component references
         public CharacterController2D controller;
         public Animator animator;
 
+        // Movement speed
         public float runSpeed = 40f;
 
-        private float _horizontalMove = 0f;
-        private bool _jump = false;
-        private bool _crouch = false;
+        // Private variables to store input
+        private float _horizontalInput = 0f;
+        private bool _isJumping = false;
+        private bool _isCrouching = false;
 
         // Update is called once per frame
-        void Update () {
+        void Update () 
+        {
+            // Get horizontal input
+            _horizontalInput = Input.GetAxisRaw("Horizontal") * runSpeed;
+            animator.SetFloat("Speed", Mathf.Abs(_horizontalInput));
 
-            _horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
-            animator.SetFloat("Speed", Mathf.Abs(_horizontalMove));
-
+            // Get jump input
             if (Input.GetButtonDown("Jump"))
             {
-                _jump = true;
+                _isJumping = true;
                 animator.SetBool("IsJumping", true);
             }
 
+            // Get crouch input
             if (Input.GetButtonDown("Crouch"))
             {
-                _crouch = true;
-            } else if (Input.GetButtonUp("Crouch"))
+                _isCrouching = true;
+            } 
+            else if (Input.GetButtonUp("Crouch"))
             {
-                _crouch = false;
+                _isCrouching = false;
             }
-
         }
 
+        // This function is called when the character lands
         public void OnLanding ()
         {
             animator.SetBool("IsJumping", false);
         }
 
+        // This function is called when the character crouches
         public void OnCrouching (bool isCrouching)
         {
             animator.SetBool("IsCrouching", isCrouching);
         }
 
+        // FixedUpdate is called at a fixed interval and is independent of frame rate
         void FixedUpdate ()
         {
-            controller.Move(_horizontalMove * Time.fixedDeltaTime, _crouch, _jump);
-            _jump = false;
+            // Move the character
+            controller.Move(_horizontalInput * Time.fixedDeltaTime, _isCrouching, _isJumping);
+            _isJumping = false;
         }
     }
 }
